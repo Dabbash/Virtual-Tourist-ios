e36a260cc2310b7957937b7b97c98c073b2183bb
“AhmedAlKharraz” <ahmed.dabbash@gmail.com>
2021-07-17 21:53:43 +0400

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Ahmed AlKharraz on 17/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
